a0332326e8b860566b268d3a07f044afcca44eb9
Josh <joshception@icloud.com>
2014-06-17 16:21:27 -0700

    ok

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,17 +10,12 @@ class TwilioController < ApplicationController
   def voice
     response = Twilio::TwiML::Response.new do |r|
       # r.Sms 'Thanks for calling :D'
-      r.Play "https://github.com/trommel/trommel.github.io/raw/master/random/1-09%20Phantoms%20Can't%20Hang.m4a",
+      r.Play 'https://github.com/trommel/trommel.github.io/raw/master/random/phantoms-cant-dance.wav',
         :loop => 0
-      # r.Play 'https://github.com/trommel/trommel.github.io/raw/master/random/never-gonna-give-you-up.mp3',
-      #   :loop => 0
     end
 
-    def messaging
-      response = Twilio::TwiML::Response.new do |r|
-        # r.Sms 'Thanks for calling :D'
-        r.Sms 'It worked :D'
-    end
+  def messaging
+  end
 
     render_twiml response
   end
