ad347e7987b68ba0f9097c050aafcd03663cf879
Josh <joshception@icloud.com>
2014-06-17 02:58:38 -0700

    stuff

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,7 +9,7 @@ class TwilioController < ApplicationController
 
   def voice
     response = Twilio::TwiML::Response.new do |r|
-      r.Sms 'Thanks for calling :D', :from => '+17786541046'
+      # r.Sms 'Thanks for calling :D'
       r.Play 'https://github.com/trommel/trommel.github.io/raw/master/random/never-gonna-give-you-up.mp3'
     end
 
