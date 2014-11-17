9543372c8a970554f0562f32924da642ff04b40b
Josh <joshception@icloud.com>
2014-11-16 23:02:37 -0800

    whoops

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -10,7 +10,7 @@ class TwilioController < ApplicationController
 
   def voice
     response = Twilio::TwiML::Response.new do |r|
-      r.Play 'https://github.com/trommel/trommel.github.io/raw/master/random/never-gonna-give-you-up.mp3',
+      r.Play 'https://github.com/trommel/trommel.github.io/raw/master/random/never_gonna_give_you_up.mp3',
       loop: 0
     end
     render_twiml response
